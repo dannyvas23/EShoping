bcad5266b1471d5e35f3df72a5cedfe695877877
Danny Vasquez <dannyvas15@gmail.com>
2024-02-25 21:33:04 -0500

    Establecer la configuracion y nombres de bases para cargar los catalogos semilla en mongoDB

---
+++ Catalog/Catalog.Infraestructure/Data/ICatalogContextSeed.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Infraestructure.Data;
+
+public interface ICatalogContextSeed
+{
+    IMongoCollection<Product> Products { get; }
+    IMongoCollection<ProductBrand> Brands { get; }
+    IMongoCollection<ProductType> Types { get; }
+}
