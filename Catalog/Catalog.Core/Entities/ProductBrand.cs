5bb614d0f2f8a57f7880cc417a4c8bda42ce875e
Danny Vasquez <dannyvas15@gmail.com>
2024-02-25 19:23:04 -0500

    BaseEntity para modelo Producto --> BDMongo, relacionado con Marca y Tipo

---
+++ Catalog/Catalog.Core/Entities/ProductBrand.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Core.Entities;
+
+public class ProductBrand
+{
+    [BsonElement("Name")] 
+    public string Name { get; set; }
+}
