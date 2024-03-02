5ddbe59bf94875a5961d27ac93afe99591fe1b72
Danny Vasquez <dannyvas15@gmail.com>
2024-03-02 17:50:28 -0500

    Dev --> Controlador API del Catalogo (Registro Handler), StartUp Registration, configuracion Swagger y versionamiento de api

---
+++ Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers;
+
+public class CatalogController: ApiController
+{
+    private readonly IMediator _mediator;
+    public CatalogController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("[action]/{id}", Name = "GetProductById")]
+    [ProducesResponseType(typeof(ProductResponse), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ProductResponse>> GetProductById(string id)
+    {
+        var query = new GetProductByIdQuery(id);
+        var result = await _mediator.Send(query);
+        return Ok(result);
+
+    }
+}
--- Catalog/Catalog.API/Startup.cs
+++ Catalog/Catalog.API/Startup.cs
@@ -1,4 +1,16 @@
-﻿namespace Catalog.API;
+﻿using System.Formats.Tar;
+using System.Reflection;
+using Asp.Versioning;
+using Catalog.Application.Handlers;
+using Catalog.Core.Repositories;
+using Catalog.Infraestructure.Data;
+using Catalog.Infraestructure.Repositories;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+
+namespace Catalog.API;
 
 public class Startup
 {
@@ -12,7 +24,35 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
-    }
+        
+        // PARA AGREGAR VERSIONAMIENTO DE API Uso de paquetes  Asp.Versioning.Mvc y Asp.Versioning.Mvc.ApiExplorer
+        //https://www.youtube.com/watch?v=1twgnDryEg4
+        services.AddApiVersioning(opt =>
+        {
+            opt.DefaultApiVersion = new ApiVersion(1, 0);
+            opt.AssumeDefaultVersionWhenUnspecified = true;
+            opt.ReportApiVersions = true;
+            opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+        });
+        
+        //La anterior libreria para usar versionamiento solo llama a 
+        //services.AddApiVersioning();
+        
+        services.AddHealthChecks()
+            .AddMongoDb(Configuration["DatabaseSettings:ConnectionString"], "Catalog Mongo Db Health Check",
+                HealthStatus.Degraded);
+        services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" }); });
+        //DI
+        services.AddAutoMapper(typeof(Startup));
+            //services.AddMediatR(typeof(CreateProductHandler).GetTypeInfo().Assembly); //--Anteriores versiones, en la prox linea es la sintaxis para registrar
+            //todos los handlers, comandos, consultas, etc dentro del ensamblado especificado al iniciar la aplicación.
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddScoped<ICatalogContextSeed, CatalogContextSeed>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IBrandRepository, ProductRepository>();
+        services.AddScoped<ITypeRepository, ProductRepository>();
+
+        }
 
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -20,6 +60,8 @@ public class Startup
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));   
         }
 
         app.UseRouting();
@@ -28,6 +70,11 @@ public class Startup
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
+            endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
         });
     }
 
